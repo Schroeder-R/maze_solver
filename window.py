220e2c9010422b3105b844664f72d10d876935ae
Schroeder-R <Schroeder357@gmail.com>
2025-02-04 19:44:17 -0600

    Initial Commit

---
+++ window.py
@@ -0,0 +1,25 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window():
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.root = Tk()
+        self.root.title("Maze Solver")
+        self.canvas = Canvas(self.root, width=width, height=height, bg="white")
+        self.canvas.pack()
+        self.isRunning = False
+        self.root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def redraw(self):
+        self.root.update_idletasks()
+        self.root.update()
+
+    def wait_for_close(self):
+        self.isRunning = True
+        while self.isRunning:
+            self.redraw()
+    def close(self):
+        self.isRunning = False
+    
+        
